60d25afded5d1b3fb96ec71aa6f075ada6f9ab5f
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-19 20:44:04 +0500

    Updated

---
+++ app.js
@@ -0,0 +1,46 @@
+
+// =====Chap No 4 ===== 
+
+// =====Chap No 4 (Q.1)=====
+// let a = 1, b = 2, c = 3;
+
+// =====Chap No 4 (Q.2)=====
+// 5 legal variablr name
+// 1 fullName
+// 2 Price
+// 3 totalAmmount
+// 4 $dollarSign
+// 5 book
+
+// // 5 illegal variablr name
+// 1 let
+// 2 var
+// 3 count
+// 4 123
+// 5 class
+
+// =====Chap No 4 (Q.3)======
+// ??????????????
+
+
+// =====Chap No 5======
+
+// =====Chap No 5 (Q.1)======
+// let a = 5;
+// let b = 3;
+// document.write(a + b)
+
+// =====Chap No  (Q.2)======
+// let a = 3;
+// let b = 5;
+// document.write(a - b)
+// document.write(a * b)
+// document.write(a / b)
+
+// =====Chap No  (Q.3)======
+
+
+
+
+
+
