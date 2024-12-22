15e1792e363286fdb4118f969270037397960155
Waqarghori <waqar.iqbal789@gmail.com>
2024-12-22 22:33:28 +0500

    updated

--- app.js
+++ app.js
@@ -20,8 +20,11 @@
 // 5 class
 
 // =====Chap No 4 (Q.3)======
-// ??????????????
-
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("Variable names can only contain Numbers, $,_For example $my_1stVariable <br>")
+// document.write("Variables must begin with a __, __ or___. For example $name, _name or name <br>")
+// document.write("variables names are case sensitive <br>")
+// document.write("variables names should not be JS keyword")
 
 // =====Chap No 5======
 
@@ -38,6 +41,17 @@
 // document.write(a / b)
 
 // =====Chap No  (Q.3)======
+// document.write(" <br><br><br><br>value after variable declaration is undefined <br>")
+// let num1 = 5
+// document.write("initial value: " + num1) 
+// document.write("<br>value after increment: "+ ++num1)
+// document.write("<br>value after addition: "+ (num1 = num1 + 7))
+// document.write("<br>value after decrement: "+ --num1)
+// document.write("<br>the reminder is: "  + (num1 % 2))
+// let ticket = 600;
+// document.write(`<br><br><br><br>total cost to buy 5 tickets to a movie is ${ticket * 5}`)
+// let x = 2, y = 1;
+// document.write(`<br><br><br><br>var result = ${--x - --y + ++x + y--}`);
 
 
 
